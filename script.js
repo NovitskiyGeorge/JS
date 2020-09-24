af9f6643e333274d1d38ba8d3c80c58795d1eb59
George <novitskiy.gs@gmail.com>
2020-09-24 15:35:23 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+// let persone = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(persone['name']);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+// console.log(arr[0]);
+// alert('hello!');
+
+// let answer = confirm('Are you here?');
+
+// console.log(answer);
+
+// let answer = +prompt('Есть ли вам 18?', 'да');
+
+// console.log(typeof(answer));
+
+// console.log('arr' + '- object');
+// console.log(3 + '- object');
+// console.log(3 + 2 + +'- object');
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(incr);
+console.log(decr);
+
+console.log('2' === 2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked && isClose);
